bfc32ae30e0673be68aa1b0d49aed19f38eab248
soruly <soruly@gmail.com>
2018-02-18 16:49:33 +0000

    Put each translation in one line

--- build.js
+++ build.js
@@ -13,9 +13,9 @@ const c = new MariaClient({
 
 c.query("SELECT * FROM anilist_chinese", (error, rows) => {
   const db = rows.map(e=>({id: parseInt(e.id), title: JSON.parse(e.json).title.chinese}));
-  const db_str = JSON.stringify(db, null, 2).replace(/"id":/g, "id:").replace(/"title":/g, "title:");
+  const db_str = db.map(e=>JSON.stringify(e).replace(/"id":/g, "id:").replace(/"title":/g, "title:")).join("\n");
   const template = fs.readFileSync("template.js", "utf8");
-  const js_str = template.replace("var database = [];", `var database = ${db_str};`);
+  const js_str = template.replace("var database = [];", "var database = [\n"+db_str+"\n];");
   fs.writeFileSync("anilist-chinese.user.js", js_str);
 });
 
