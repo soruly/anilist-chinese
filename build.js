8f86c2db931b9d17ce7045c3f78694e079bce3d3
soruly <soruly@gmail.com>
2018-02-21 02:02:20 +0000

    Skip build if the generated js code is the same

--- build.js
+++ build.js
@@ -15,12 +15,17 @@ c.query("SELECT * FROM anilist_chinese", (error, rows) => {
   const db = rows.map(e=>({id: parseInt(e.id), title: JSON.parse(e.json).title.chinese}));
   const db_str = db.map(e=>JSON.stringify(e).replace(/"id":/g, "id:").replace(/"title":/g, "title:")).join(",\n");
   const template = fs.readFileSync("template.js", "utf8");
-  const d = new Date();
-  const version_str = `1.${d.getFullYear()}.${d.getMonth()+1}.${d.getDate()}`;
-  const js_str = template
-    .replace("var database = [];", "var database = [\n"+db_str+"\n];")
-    .replace("// @version      1.0", `// @version      ${version_str}`);
-  fs.writeFileSync("anilist-chinese.user.js", js_str);
+  const js_str = template.replace("var database = [];", "var database = [\n"+db_str+"\n];");
+  if (js_str !== fs.readFileSync("cache.txt", "utf8")) {
+    fs.writeFileSync("cache.txt", js_str);
+    const d = new Date();
+    const version_str = `1.${d.getFullYear()}.${d.getMonth()+1}.${d.getDate()}`;
+    const js_file_str = js_str.replace("// @version      1.0", `// @version      ${version_str}`);
+    fs.writeFileSync("anilist-chinese.user.js", js_file_str);
+    console.log(`build completed (version ${version_str})`);
+  } else {
+    console.log("build is the same, js file not updated.");
+  }
 });
 
 c.end();
