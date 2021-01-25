3b5e32e7f51b2bf64a8e1cf4e21725cd75f8d874
soruly <soruly@gmail.com>
2021-01-25 22:26:26 +0800

    Use top-level await

--- build.js
+++ build.js
@@ -14,30 +14,28 @@ const knex = Knex({
   },
 });
 
-(async () => {
-  const animeIDList = await knex("anilist_chinese").select("*");
-  const db = animeIDList.map((e) => ({
-    id: parseInt(e.id, 10),
-    title: JSON.parse(e.json).title.chinese,
-  }));
-  const db_str = db
-    .map((e) =>
-      JSON.stringify(e)
-        .replace(/"id":/g, "id:")
-        .replace(/"title":/g, "title:")
-    )
-    .join(",\n");
-  const template = fs.readFileSync("template.js", "utf8");
-  const js_str = template.replace("var database = [];", `var database = [\n${db_str}\n];`);
-  if (js_str !== fs.readFileSync("cache.txt", "utf8")) {
-    fs.writeFileSync("cache.txt", js_str);
-    const d = new Date();
-    const version_str = `2.${d.getFullYear()}.${d.getMonth() + 1}.${d.getDate()}`;
-    const js_file_str = js_str.replace("// @version      2.0", `// @version      ${version_str}`);
-    fs.writeFileSync("anilist-chinese.user.js", js_file_str);
-    console.log(`build completed (version ${version_str})`);
-  } else {
-    console.log("build is the same, js file not updated.");
-  }
-  knex.destroy();
-})();
+const animeIDList = await knex("anilist_chinese").select("*");
+const db = animeIDList.map((e) => ({
+  id: parseInt(e.id, 10),
+  title: JSON.parse(e.json).title.chinese,
+}));
+const db_str = db
+  .map((e) =>
+    JSON.stringify(e)
+    .replace(/"id":/g, "id:")
+    .replace(/"title":/g, "title:")
+  )
+  .join(",\n");
+const template = fs.readFileSync("template.js", "utf8");
+const js_str = template.replace("var database = [];", `var database = [\n${db_str}\n];`);
+if (js_str !== fs.readFileSync("cache.txt", "utf8")) {
+  fs.writeFileSync("cache.txt", js_str);
+  const d = new Date();
+  const version_str = `2.${d.getFullYear()}.${d.getMonth() + 1}.${d.getDate()}`;
+  const js_file_str = js_str.replace("// @version      2.0", `// @version      ${version_str}`);
+  fs.writeFileSync("anilist-chinese.user.js", js_file_str);
+  console.log(`build completed (version ${version_str})`);
+} else {
+  console.log("build is the same, js file not updated.");
+}
+knex.destroy();
