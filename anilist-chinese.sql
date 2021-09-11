4d2c703e8ce457a161581757204833d3f1579124
soruly <soruly@gmail.com>
2021-09-11 23:10:05 +0800

    Update titles database

--- anilist-chinese.sql
+++ anilist-chinese.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.5.11-MariaDB, for Linux (x86_64)
+-- MariaDB dump 10.19  Distrib 10.5.12-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: anime
 -- ------------------------------------------------------
--- Server version	10.5.11-MariaDB
+-- Server version	10.5.12-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
