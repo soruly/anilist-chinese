678b829f6220252537791eec4f6c039a7c7298b2
soruly <soruly@gmail.com>
2025-09-13 04:51:04 +0000

    Update titles database

--- anilist-chinese.sql
+++ anilist-chinese.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict EYhAA9wIjayShmNqnS0zl5PpooHQdHpPQSNuwtdR1Hcix8iFWxRfBhpDH68C8xU
+\restrict CFzthtn6Sj6bvqlZPZVre0X0PbzIWVUiDxVYabiDhAc31ajDwDSdwvYDuxa21UA
 
 -- Dumped from database version 17.6 (Debian 17.6-1.pgdg13+1)
 -- Dumped by pg_dump version 17.6 (Debian 17.6-1.pgdg13+1)
@@ -7703,5 +7703,5 @@ ALTER TABLE ONLY public.anilist_chinese
 -- PostgreSQL database dump complete
 --
 
-\unrestrict EYhAA9wIjayShmNqnS0zl5PpooHQdHpPQSNuwtdR1Hcix8iFWxRfBhpDH68C8xU
+\unrestrict CFzthtn6Sj6bvqlZPZVre0X0PbzIWVUiDxVYabiDhAc31ajDwDSdwvYDuxa21UA
 
